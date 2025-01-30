f8a5bafa90a248345daeee389b6c7ababb5e381f
Felipe Burin <felipe.burin-ext@ab-inbev.com>
2025-01-30 15:46:29 -0300

    Adding core - usecase for cryto valuation

--- src/main/java/com/template/cleanarchitecture/usecase/cryptoaddressvaluation/GetCryptoValuationUseCase.java
+++ src/main/java/com/template/cleanarchitecture/usecase/cryptoaddressvaluation/GetCryptoValuationUseCase.java
@@ -1,7 +1,27 @@
 package com.template.cleanarchitecture.usecase.cryptoaddressvaluation;
 
 import com.template.cleanarchitecture.domain.cryptoaddressvaluation.gateway.CryptoAddressValuationGateway;
+import com.template.cleanarchitecture.usecase.cryptoaddress.GetCryptoAddressUseCase;
+import com.template.cleanarchitecture.usecase.cryptoaddress.dto.CryptoAddressRequest;
+import com.template.cleanarchitecture.usecase.cryptoaddressvaluation.dto.CryptoValuationRequest;
+import com.template.cleanarchitecture.usecase.cryptoaddressvaluation.dto.CryptoValuationResponse;
 
 public class GetCryptoValuationUseCase {
-    private CryptoAddressValuationGateway gateway;
+    private final CryptoAddressValuationGateway gateway;
+    private final GetCryptoAddressUseCase getCryptoAddressUseCase;
+
+    public GetCryptoValuationUseCase(
+            final CryptoAddressValuationGateway gateway,
+            final GetCryptoAddressUseCase getCryptoAddressUseCase) {
+        this.gateway = gateway;
+        this.getCryptoAddressUseCase = getCryptoAddressUseCase;
+    }
+
+    public CryptoValuationResponse getValuation(final CryptoValuationRequest request) {
+        final var cryptoAddressRequest = new CryptoAddressRequest(request.cryptoType(), request.address());
+        final var cryptoAddress = getCryptoAddressUseCase.get(cryptoAddressRequest);
+        final var valuation = gateway.get(request.cryptoType(), request.address());
+
+        return new CryptoValuationResponse(cryptoAddress.cryptoType(), cryptoAddress.address(), valuation.value());
+    }
 }
